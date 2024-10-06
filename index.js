34e96ebf6831271abe9a4ec229cb3cd172acc57d
Cliff Fajardo <cliff.fajardo@gmail.com>
2024-10-05 19:29:54 -0700

    refactoring routes, add configuration files, static files

--- index.js
+++ index.js
@@ -1,89 +1,33 @@
+const startupDebugger = require('debug')('app:startup');
+const config = require('config');
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi');
 const express = require('express');
+
+const courses = require('./routes/courses');
+const home = require('./routes/home');
+
 const app = express();
+
+// Configuration
+console.log(`Application Name: ${ config.get('name')}`);
+console.log(`Application Name: ${ config.get('mail.host')}`);
+
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    startupDebugger('Morgan enabled...');
+}
+
 app.use(express.json());
-
-const courses = [
-    { id: 1, name: 'course1'},
-    { id: 2, name: 'course2'},
-    { id: 3, name: 'course3'}
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-});
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-
-    if(!course) return res.status(404).send(`The course with ID: ${req.params.id} was not found`);
-    res.send(course);
-});
-
-
-// Add New Course
-app.post('/api/courses', (req, res) => {
-
-    const { error } = validateCourse(req.body); // { error } - object destructuring
-    if(error) return res.status(400).send(result.error.details[0].message);
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course);
-    res.send(course);
-});
-
-
-// Update Existing Course
-app.put('/api/courses/:id', (req, res) => {
-    //check if course exists
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-
-    if(!course) return res.status(404).send(`The course with ID: ${req.params.id} was not found`);
-
-    //validate course
-    const { error } = validateCourse(req.body); // { error } - object destructuring
-    if(error){
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-
-    //update course
-    course.name = req.body.name;
-    res.send(course);
-
-});
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use(helmet());
+app.use('/api/courses', courses);
+app.use('/', home);
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
 });
 
-
-// Delete a course
-app.delete('/api/courses/:id', (req, res) => {
-    //check if course exists
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send(`The course with ID: ${req.params.id} was not found`);
-
-    // delete
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-
-    res.send(course);
-});
-
-
-function validateCourse(course) {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-
-   return schema.validate(course);
-}
